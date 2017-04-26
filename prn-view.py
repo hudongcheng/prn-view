ae38e515feb296e1b5fb219dead2880bf5ab5fbf
Daniel Hu <hdc@pantum.com>
2017-04-26 15:20:08 +0800

    add Ctl+C to end up

--- prn-view.py
+++ prn-view.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from socket import *
-from time import ctime
+import time
 
 HOST = ''
 PORT = 9100
@@ -10,19 +10,24 @@ ADDR = (HOST,PORT)
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
-t = 0
+#t = ""
 while True:
 	print 'waiting for connection ...'
-	tcpCliSock, addr = tcpSerSock.accept()
-	print '... connected from: ', addr
-	prn_file = open('prn_' + str(t) + '.prn', 'w')
-	t = t + 1
-	while True:
-		data = tcpCliSock.recv(BUFSIZ)
-		if not data:
-			break
-		prn_file.write(data)
-	print 'write to ' + prn_file.name
-	prn_file.close()
-	tcpCliSock.close()
+	try:
+		tcpCliSock, addr = tcpSerSock.accept()
+		print '... connected from: ', addr
+		t = time.strftime('%Y%m%d%H%M%S', time.localtime())
+		prn_file = open('prn_' + t + '.prn', 'w')
+		while True:
+			data = tcpCliSock.recv(BUFSIZ)
+			if not data:
+				break
+			prn_file.write(data)
+		print 'write to ' + prn_file.name
+		prn_file.close()
+		tcpCliSock.close()
+	except KeyboardInterrupt, e:
+		tcpSerSock.close()
+		exit(-1)
+
 tcpSerSock.close()
